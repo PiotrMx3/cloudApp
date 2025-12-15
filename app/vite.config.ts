a918f1bbf5dd47af28f37e47b42ef6a2e2da0756
Piotr Mulinski <piotr.mulinski@icloud.com>
2025-12-15 23:15:29 +0100

    vite host update

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: true,
+    allowedHosts: ["cloudapp.piotrmulinski.com"],
     host: true,
     port: 4173,
   },
